bd3804a3d2193bade526775bbb4bc386ca7863c7
ibosque <64149579+ibosque@users.noreply.github.com>
2020-04-24 13:33:28 +0200

    force vector  written as a vector directly
    also some spaces corrected

--- HW7.m
+++ HW7.m
@@ -7,22 +7,27 @@ close all
 %acc of angles, Symb_angels.m gives the starting position of
 %all angles, symb_slid gives slider acceleration)
 TMT_method()
-theta2 = 0;
 w = 75;
+
+theta2 = 0;
 theta2d = w/60*2*pi;
+
 start_angels;
+
 theta4  = angels(1);
 theta4d = angels(2);
 theta5  = angels(3);
 theta5d = angels(4);
+
 yi = [theta2; theta2d; theta4; theta4d; theta5; theta5d];
-n =8;
+n = 8;
 t0 = 0;
 tfinal = 1.8; % two rounds is about 1.7... sec so i take 1.8s
 dt = tfinal/2^n;
 ydot = calcydot(yi);
 idx = 0;
 time = 0:dt:tfinal;
+
 %% RK4 method with coordinate projection
 for idx = 1:length(time)
 %calculate acceleration of angles, lambda's and slider
@@ -32,15 +37,22 @@ for idx = 1:length(time)
 [k4] = calcydot(yi(:,idx) + dt*k3);
 ydi(:,idx) = 1/6*(k1+2*k2+2*k3+k4);
 yi(:,idx+1) = yi(:,idx) + dt*ydi(:,idx);
-% yi(:,idx+1) = yi(:,idx) + dt*k1; Euler method
+
 % Coordinate Projection Method
 
-O2A = 0.2; O4B = 0.7; BC = 0.6;
-O4O2 = 0.3; O4G4 = 0.4; BG5 = 0.3; yC = 0.9;
+O2A  = 0.2; 
+O4B  = 0.7; 
+BC   = 0.6;
+O4O2 = 0.3; 
+O4G4 = 0.4; 
+BG5  = 0.3; 
+yC   = 0.9;
+
 % Constrains from TMT_method copy
 C1 = yC - O4B*sin(yi(3,idx))-BC*sin(yi(5,idx));
 C2 = O2A*cos(yi(1,idx)) - sqrt(((O2A*cos(yi(1,idx)))^2 + (O4O2+O2A*sin(yi(1,idx)))^2))*cos(yi(3,idx));
 Constraints = [C1;C2];
+
 %jacobian copy of constrains
 CCd = [0,-(7*cos(yi(3,idx)))/10, -(3*cos(yi(5,idx)))/5; (cos(yi(3,idx))*((2*cos(yi(1,idx))*sin(yi(1,idx)))/25 - (2*cos(yi(1,idx+1))*(sin(yi(1,idx))/5 + 2/5))/5))/(2*(cos(yi(1,idx))^2/25 + (sin(yi(1,idx))/5 + 2/5)^2)^(1/2)) - sin(yi(1,idx))/5, sin(yi(3,idx))*(cos(yi(1,idx))^2/25 + (sin(yi(1,idx))/5 + 2/5)^2)^(1/2), 0];
 Ce = CCd.'*inv(CCd*CCd.'); % making C plus
@@ -61,14 +73,15 @@ title(' Angle speed ')
 xlabel('Time [sec] ')
 ylabel('Speed rad/s')
 legend('\theta 2', '\theta 3/4','\theta 5')
-figure(2);% sliding speed question c
+
+% Question C, sliding speed
+figure(2);
 subplot(2,2,1);
 plot(time,s1(1,:)); hold on
 title(' Slider 3 & rocker 4 ')
 xlabel('Time [sec] ')
 ylabel('Speed rad/s')
 subplot(2,2,2);
-
 plot(time,s1(2,:));
 title(' Horizontal position slider 6 ')
 xlabel('Time [sec] ')
@@ -83,16 +96,16 @@ plot(time,s1(4,:));
 title(' Acceleration slider 6 ')
 xlabel('Time [sec] ')
 ylabel('Acceleration [m/s^2]')
-figure(3); % force plot question d
-% plot(time,sqrt(Cforce(1,:).^2+Cforce(2,:).^2)); hold on
+
+% Question D, force plot
+figure(3); 
 plot(time,(Cforce(1,:))); hold on
 plot(time,Cforce(3,:));
-% plot(time,Cforce(2,:));
 title(' Normal forces ')
 xlabel('Time [sec] ')
 ylabel('Force [N]')
 legend('Slider 3 on 4','Slider 6')
-% plot(time,Cforce(1,:));
+
 %% simulation pot ctr+t to use
 % Use after first run, running while
 %
@@ -131,12 +144,13 @@ ylim([-0.5,1.5])
 end
 function [ydot, labda, Slider3] = calcydot(yi)
 % yi, current value -> to ydot velocity and acc
-theta2 = yi(1);
+theta2  = yi(1);
 theta2d = yi(2);
-theta4 = yi(3);
+theta4  = yi(3);
 theta4d = yi(4);
-theta5 = yi(5);
+theta5  = yi(5);
 theta5d = yi(6);
+  
 symb_Acc ;
 theta2dd = Acc(1);
 theta4dd = Acc(2);
@@ -152,14 +166,34 @@ end
 function TMT_method()
 syms theta2 theta4 theta5
 syms theta2d theta4d theta5d theta2dd theta4dd theta5dd % for dx/dt I use xd etc
-O2A = 0.2; O4B = 0.7; BC = 0.6;
-O4O2 = 0.3; O4G4 = 0.4; BG5 = 0.3; yC = 0.9;
-m3= 0.5; m4= 6; m5= 4; m6= 2; J4=10; J5= 6; F=1000;
-T=0; J2= 100; w= 75*2*pi/60; J3=0;
+O2A  = 0.2; 
+O4B  = 0.7; 
+BC   = 0.6;
+O4O2 = 0.3; 
+O4G4 = 0.4; 
+BG5  = 0.3; 
+yC   = 0.9;
+
+m3 = 0.5; 
+m4 = 6;
+m5 = 4; 
+m6 = 2; 
+
+J2 = 100;
+J3 = 0;
+J4 = 10; 
+J5 = 6; 
+
+F = 1000;
+T = 0; 
+ 
+w = 75*2*pi/60; 
+
 %setting up kinematics.
-q = [theta2;theta4;theta5];
-qd = [theta2d;theta4d;theta5d];
+q   = [theta2;theta4;theta5];
+qd  = [theta2d;theta4d;theta5d];
 qdd = [theta2dd; theta4dd; theta5dd];
+
 xA = O2A*cos(theta2);
 yA = O4O2+O2A*sin(theta2);
 xG4= O4G4*cos(theta4);
@@ -167,6 +201,7 @@ yG4= O4G4*sin(theta4);
 xG5= O4B*cos(theta4)+BG5*cos(theta5);
 yG5= O4B*sin(theta4)+BG5*sin(theta5);
 xC = O4B*cos(theta4)+BC*cos(theta5);
+
 %making Ti,k matrix
 Ki = [theta2;xA;yA;theta4;xG4;yG4;theta4;xG5;yG5;theta5;xC];%all kinematics combined
 Ti = jacobian(Ki, q);
@@ -175,7 +210,7 @@ gk = jacobian(Tiv,q)*qd;
 Tacc = jacobian(Tiv,qd)*qdd + jacobian(Tiv,q)*qd;
 %making Mass and gravity matrix and vector
 M = diag([J2 m3 m3 J3 m4 m4 J4 m5 m5 J5 m6]);
-% Constrains
+% Constraints
 C1 = yC - O4B*sin(theta4)-BC*sin(theta5);
 C2 = O2A*cos(theta2)-sqrt((xA^2+yA^2))*cos(theta4);
 Constraints = [C1;C2];
@@ -183,9 +218,8 @@ CCd = jacobian(Constraints,q);
 CCdd = jacobian(CCd*qd,q)*qd;
 
 %Forces
-fi = zeros(11,1);
-fi(1) = T;
-fi(11) = F;
+fi = [T; 0; 0; 0; 0; 0; 0; 0; 0; 0; F;];
+
 %making TMT matrix
 TMT = simplify([Ti.'*M*Ti CCd.';CCd zeros(2,2)]);
 % dTdot = TMT
